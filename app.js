90fb088473ff85d6f01699cd733223f2dadd4789
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-07-26 17:01:06 +0200

    IndexedDB manual: creación y actualización

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-
 // indexedDB: Reforzamiento
+/*
+Vamos a crear manualmente la base de datos local. Es muy poco intuitivo pero es necesario
+conocerlo. Más adelante crearemos la base de datos con librerías de teceros
 
+Es poco intuitivo porque los request y los listeners los debemos manejar manualmente,
+lo cual es muy tedioso
+*/
 
+// Creamos una variable que almacena la base de datos
+const request = window.indexedDB.open('mi-database', 1);
 
+// Se actualiza la base de datos cuando se crea por primera vez o se sube la versión
+// Se recomienda subir de versión sólo cuando sea absolutamente necesario
+request.onupgradeneeded = event => {
+
+    console.log('Actualización de la BD');
+
+    // Localizamos el request de actualización y lo almacenamos en una variable
+    const db = event.target.result;
+
+    // Debemos crear una llave para manejar los registros en la base de datos
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+    });
+
+};
 
